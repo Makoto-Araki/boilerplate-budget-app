4ec7abd4957d4ac644b1b657d6cc8e6219b8766f
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2022-10-20 01:05:55 +0000

    10月20日 - 10:00

--- budget.py
+++ budget.py
@@ -1,188 +1,62 @@
-'''
-Class - Category
-'''
-
 class Category:
-    
-    '''
-    Class Constructor
-    '''
     
     def __init__(self, name):
         self.name = name
         self.ledger = list()
+
+    def __str__(self):
+        self.get_balance()
     
-    '''
-    Class Method - deposit
-    '''
+    def deposit(self, amount, description = ''):
+        self.ledger.append({'amount': amount, 'description': description})
     
-    def deposit(self, amount, *description):
-        
-        '''
-        Change object with variable parameters
-        '''
-        
-        if len(description) > 0:
-            if isinstance(description, tuple):
-                self.ledger.append({'amount': amount, 'description': description[0]})
-            elif isinstance(description, str):
-                self.ledger.append({'amount': amount, 'description': description})
-        else:
-            self.ledger.append({'amount': amount, 'description': ''})
-    
-    '''
-    Class Method - withdraw
-    '''
-    
-    def withdraw(self, amount, *description):
-        
-        '''
-        Check if withdrawal is possible
-        '''
-        
+    def withdraw(self, amount, description = ''):
         if not self.check_funds(amount):
             return False
         else:
-            
-            '''
-            If amount is positive, convert to negative
-            '''
-            
-            if amount > 0:
-                amount = amount - amount * 2
-            
-            '''
-            Change registration object with variable parameters
-            '''
-            
-            if len(description) > 0:
-                self.ledger.append({'amount': amount, 'description': description})
-            else:
-                self.ledger.append({'amount': amount})
-            
+            if amount > 0: amount = amount * -1
+            self.ledger.append({'amount': amount, 'description': description})
             return True
     
-    '''
-    Class Method - get_balance
-    '''
-    
     def get_balance(self):
-        
-        '''
-        Initialize the variables
-        '''
-        
-        total = 0
+        return_str = ''
         padding_length = int((30 - len(self.name)) / 2)
-        
-        '''
-        Title
-        '''
-        
         if len(self.name) % 2 == 0:
             temp1 = '*' * padding_length
             temp2 = '*' * padding_length
+            return_str += temp1 + self.name + temp2 + '\n'
         else:
             temp1 = '*' * padding_length
             temp2 = '*' * (padding_length + 1)
+            return_str += temp1 + self.name + temp2 + '\n'
         
-        print(temp1 + self.name + temp2)
-        
-        '''
-        Iterate over a list in an object
-        '''
-        
-        for i in range(len(self.ledger)):
+        total = 0
+        for item in self.ledger:
+            temp3 = item['description'][0:23].ljust(23, ' ') if item['description'] != '' else ' ' * 23
+            temp4 = str('{:6.2f}'.format(item['amount'])).rjust(7, ' ')
+            total += item['amount']
+            return_str += temp3 + temp4 + '\n'
             
-            '''
-            Description + Amount
-            '''
-            #print(self.ledger[i])
-            if isinstance(self.ledger[i]['description'], tuple):
-                temp3 = self.ledger[i]['description'][0][0:23].ljust(23, ' ')
-            elif isinstance(self.ledger[i]['description'], str):
-                temp3 = self.ledger[i]['description'][0:23].ljust(23, ' ')
-            elif 'description' in self.ledger[i]:
-                temp3 = ' ' * 23
-            
-            temp4 = self.ledger[i]['amount']
-            temp4 = str('{:6.2f}'.format(temp4))
-            temp4 = temp4.rjust(7, ' ')
-            total += self.ledger[i]['amount']
-            
-            print(temp3 + temp4)
-            
-        '''
-        Total
-        '''
-        
         total_str = str('{:6.2f}'.format(total))
-        print('Total: ' + total_str)
+        return_str += 'Total: ' + total_str + '\n'
+        return return_str
         
-        '''
-        Return value
-        '''
-        
-        return total
-        
-    '''
-    Class Method - transfer
-    '''
-    
     def transfer(self, amount, Instance):
-        
-        '''
-        Check if withdrawal is possible
-        '''
-        
         if not self.check_funds(amount):
             return False
         else:
-            
-            '''
-            Withdraw executed
-            '''
-            
             self.withdraw(amount, 'Transfer to ' + Instance.name)
-            
-            '''
-            Destination category deposits
-            '''
-            
             Instance.deposit(amount, 'Transfer from ' + self.name)
-            
-            '''
-            Return value
-            '''
-            
             return True
-    
-    '''
-    Method - check_funds
-    '''
     
     def check_funds(self, amount):
-        
-        '''
-        Totalling the category amount
-        '''
-        
         total = 0
-        for i in range(len(self.ledger)):
-            total += self.ledger[i]['amount']
-        
-        '''
-        Check if withdrawal is possible
-        '''
-        
+        for item in self.ledger:
+            total += item['amount']
         if total < amount:
-            return False
+          return False
         else:
-            return True
-
-'''
-Unknown
-'''
+          return True
 
 def create_spend_chart(categories):
-  print(categories)
+    print('Hello World')
--- main.py
+++ main.py
@@ -3,36 +3,25 @@ import budget
 from budget import create_spend_chart
 from unittest import main
 
-#food = budget.Category("Food")
-#food.deposit(1000, "initial deposit")
-#food.withdraw(10.15, "groceries")
-#food.withdraw(15.89, "restaurant and more food for dessert")
-#print(food.get_balance())
-#clothing = budget.Category("Clothing")
-#food.transfer(50, clothing)
-#clothing.withdraw(25.55)
-#clothing.withdraw(100)
-#auto = budget.Category("Auto")
-#auto.deposit(1000, "initial deposit")
-#auto.withdraw(15)
-#print(food.get_balance()) # add
-#print(clothing.get_balance()) # add
-#print(auto.get_balance()) # add
-#print(food)
-#print(clothing)
-
-#entertainment = budget.Category("Entertainment")
-#food.deposit(900, "deposit")
-#food.withdraw(45.67, "milk, cereal, eggs, bacon, bread")
-#food.transfer(20, entertainment)
-#food.get_balance()
-'''
-*************Food*************
-deposit                 900.00
-milk, cereal, eggs, bac -45.67
-Transfer to Entertainme -20.00
-Total: 834.33
-'''
+food = budget.Category("Food")
+food.deposit(1000, "initial deposit")
+food.withdraw(10.15, "groceries")
+food.withdraw(15.89, "restaurant and more food for dessert")
+clothing = budget.Category("Clothing")
+clothing.deposit(1000, "initial deposit")
+food.transfer(50, clothing)
+clothing.withdraw(25.55, 'Others')
+clothing.withdraw(100, 'Others')
+clothing.transfer(250, food)
+auto = budget.Category("Auto")
+auto.deposit(1000, "initial deposit")
+food.transfer(50, auto)
+auto.withdraw(15, 'vehicles')
+auto.withdraw(45.25, 'airplanes')
+auto.withdraw(450, 'Others')
+print(food.get_balance())
+print(clothing.get_balance())
+print(auto.get_balance())
 
 #print(create_spend_chart([food, clothing, auto]))
 
